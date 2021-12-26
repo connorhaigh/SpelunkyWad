4b9f159f13f52697f685fa859bf4fd82c806fa36
Connor Haigh <connorhaigh@gmail.com>
2021-12-26 18:21:46 +0000

    Added some more archive writer tests

--- SpelunkyWad.Tests/ArchiveWriterTests.cs
+++ SpelunkyWad.Tests/ArchiveWriterTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SpelunkyWad.Tests
@@ -29,22 +31,47 @@ namespace SpelunkyWad.Tests
 			{
 				ArchiveWriter.Write(archive, wadStream, wixStream);
 
+				Assert.IsTrue(wadStream.ToArray().SequenceEqual(new byte[] { 27, 95, 17, 69, 10, 45, 91, 28, 3, 97, 57, 81, 8, 12, 63, 91, 76, 29 }));
+				Assert.AreEqual(string.Join(Environment.NewLine, "!group Test", "Foo 0 3", "Bar 3 6", "Baz 9 9", string.Empty), Encoding.UTF8.GetString(wixStream.ToArray()));
+			}
+		}
+
+		[TestMethod]
+		public void TestWriteSingleGroupSingleEntry()
+		{
+			var archive = new Archive();
+			archive.Groups.Add(new Group("Alpha"));
+			archive.Groups[0].Entries.Add(new Entry("File", new MemoryStream(new byte[] { 1, 2, 3 })));
+
+			using (var wadStream = new MemoryStream())
+			using (var wixStream = new MemoryStream())
+			{
+				ArchiveWriter.Write(archive, wadStream, wixStream);
+
+				Assert.AreEqual(string.Join(Environment.NewLine, "!group Alpha", "File 0 3", string.Empty), Encoding.UTF8.GetString(wixStream.ToArray()));
+			}
+		}
+
+		[TestMethod]
+		public void TestWriteMultipleGroupsSingleEntries()
+		{
+			var archive = new Archive();
+			archive.Groups.Add(new Group("Alpha"));
+			archive.Groups.Add(new Group("Beta"));
+			archive.Groups.Add(new Group("Gamma"));
+			archive.Groups[0].Entries.Add(new Entry("Foo", new MemoryStream(new byte[] { 1, 2, 3 })));
+			archive.Groups[1].Entries.Add(new Entry("Bar", new MemoryStream(new byte[] { 4, 5, 6 })));
+			archive.Groups[2].Entries.Add(new Entry("Baz", new MemoryStream(new byte[] { 7, 8, 9 })));
+
+			using (var wadStream = new MemoryStream())
+			using (var wixStream = new MemoryStream())
+			{
+				ArchiveWriter.Write(archive, wadStream, wixStream);
+
 				wadStream.Seek(0L, SeekOrigin.Begin);
 				wixStream.Seek(0L, SeekOrigin.Begin);
 
-				using (var wixStreamReader = new StreamReader(wixStream))
-				{
-					var wix = wixStreamReader.ReadToEnd();
-
-					Assert.AreEqual(
-						string.Join(Environment.NewLine,
-							"!group Test",
-							"Foo 0 3",
-							"Bar 3 6",
-							"Baz 9 9",
-							string.Empty),
-						wix);
-				}
+				Assert.AreEqual(string.Join(Environment.NewLine, "!group Alpha", "Foo 0 3", "!group Beta", "Bar 3 3", "!group Gamma", "Baz 6 3", string.Empty), Encoding.UTF8.GetString(wixStream.ToArray()));
 			}
 		}
 	}
